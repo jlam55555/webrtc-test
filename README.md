6c19a078526ebb5dcf34602280248b6e60c3dd9e
Jonathan Lam <jlam55555@gmail.com>
2018-06-25 22:14:01 -0400

    updated script with more straightforward version

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Experimenting with WebRTC technologies for peer-to-peer connections
 
 ### Description
-This creates a WebRTC peer-to-peer connection between the webpage and itself. This is in preparation for more advanced connections.
+This creates a WebRTC peer-to-peer connection between the webpage and itself. This is in preparation for more advanced connections. This corresponds to my introduction to WebRTC at my article ["Better RTC with WebRTC"](http://eis.jonathanlam.tech/posts/better-rtc-with-webrtc), and is meant to linearly show the sequence of steps necessary to setting up a WebRTC connection.
 
 This needs to be run on a web server (cannot be run statically).
 
--- main.js
+++ main.js
@@ -1,95 +1,94 @@
-// tutorial: https://www.html5rocks.com/en/tutorials/webrtc/basics/
+/**
+  * This corresponds to my introduction to WebRTC at http://eis.jonathanlam.tech/posts/better-rtc-with-webrtc
+  * @author Jonathan Lam
+  */
 
-// elements
-let videoPlayer = document.querySelector('#video-player');
+// video elements and error handling mechanism
+let videoPlayer1 = document.querySelector('#video-player-1');
 let videoPlayer2 = document.querySelector('#video-player-2');
-
-// step 1: get user media (audio and/or video)
 function handleError(err) {
-  console.log(err);
+  console.error(err);
 }
-navigator.getUserMedia({
-  video: {
-    optional: [
-      { minFrameRate: 60 },
-      { maxWidth: 500 },
-      { maxHeight: 300 }
-    ]
-  },
+
+/*
+  SET UP MEDIA AND RTCPEERCONNECTION OBJECTS
+  STEPS 1-5
+  note that createOffer (in the handshake) must occur after addStream() (in this section). More details here: https://stackoverflow.com/questions/27489881/webrtc-never-fires-onicecandidate/27758788#27758788
+*/
+/* step 1: host: obtain a stream of data */
+navigator.mediaDevices.getUserMedia({
+  video: true,
   audio: false
-}, startRTC, handleError);
+})
+  .then(hostBeginRTC)
+  .catch(handleError);
 
-// (client side is pc2)
+/*
+  step 2: host and client: match a host and a client via some signalling channel/protocol
+  (here there is no signalling channel, so the other pc's functions will be called for simplicity)
+*/
+
+/* step 3: host and client: create RTCPeerConnection objects */
+let pc1 = new RTCPeerConnection();
 let pc2 = new RTCPeerConnection();
-pc2.getIceCandidate = function(candidate) {
-  if(candidate == null) {
-    console.log('ice candidate gathering finished');
-    return;
-  }
-  console.log('testing 7 ' + candidate);
-  pc2.addIceCandidate(candidate)
-    .catch(function(error) {
-      console.log(error);
-    });
-};
-pc2.setDescription = function(desc) {
-  console.log('testing 6', desc);
-  pc2.setRemoteDescription(new RTCSessionDescription(desc));
 
-  // now send answer
-  pc2.createAnswer()
-    .then(function(answer) {
-      pc2.setLocalDescription(answer);
-      pc.setDescription(answer);
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+/* step 4: (host): add stream to the WebRTC connectionn */
+function hostBeginRTC(stream) {
+  pc1.addStream(stream);
+
+  // also show in "input" video element
+  videoPlayer1.srcObject = stream;
+
+  // handshake
+  handshake();
 }
-// once stream arrives, show it in remote video element
-pc2.ontrack = function(event) {
-  console.log('test 17 1');
-  videoPlayer2.srcObject = event.streams[0];
-}
-pc2.onaddstream = function(event) {
-  console.log('test 17 2' + event.stream);
+
+/* step 5: (client): handle stream */
+pc2.onaddstream = event => {
+  // show in "output" video element
   videoPlayer2.srcObject = event.stream;
 }
 
-// step 2: connection interaction
-// create PeerConnection object
-let pc = new RTCPeerConnection();
-function startRTC(stream) {
-  console.log('testing 1');
-  
-  // send ice candidates to other peer
-  pc.onicecandidate = function(event) {
-    pc2.getIceCandidate(event.candidate);
-  };
+/*
+  HANDLE ICE CANDIDATES
+  STEPS 6-7
+  note that ice candidates are generated when setLocalDescription() (in the handshake) is called
+*/
 
-  // show stream on current video element
-  videoPlayer.srcObject = stream;
+/* step 6: host: handle ICE candidates and send to client */
+pc1.onicecandidate = event => {
+  /* step 7: client: handle ice candidates sent by the host */
+  if(event.candidate !== null)
+    pc2.addIceCandidate(event.candidate)
+      .catch(handleError);
+}
 
-  // add stream to PeerConnection
-  console.log('testing 2');
-  pc.addStream(stream);
+/*
+  ESTABLISH THE HANDSHAKE
+  STEPS 8-12
+*/
+function handshake() {
+  /* step 8: host: create offer */
+  pc1.createOffer()
+    .then(offer => {
+      pc1.setLocalDescription(offer)
+        .catch(handleError);
 
-  // create offer
-  console.log('testing 3');
-  pc.createOffer()
-    .then(function(offer) {
-      console.log('testing 4');
-      pc.setLocalDescription(offer)
-        .catch(console.log);
-      pc2.setDescription(offer);
-    })
-    .catch(function(error) {
-      console.log('error: ' + error);
+      /* step 9: host: send host's SDP description to client via signalling channel */
+      pc2.setRemoteDescription(offer)
+        .catch(handleError);
+
+      /* step 10: client: receive offer and create answer */
+      pc2.createAnswer()
+        .then(answer => {
+          
+          /* step 11: client: send client's SDP offer and create answer */
+          pc2.setLocalDescription(answer);
+
+          /* step 12: host: receive answer */
+          pc1.setRemoteDescription(answer)
+            .catch(handleError);
+
+        });
     });
-
-  pc.setDescription = function(answer) {
-    pc.setRemoteDescription(answer)
-      .catch(console.log);
-  };
-
 }
